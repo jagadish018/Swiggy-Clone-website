3968f02ae51172263d8d35c7de8d5dd3f79d51da
jagadishck018 <jagadishck.018@gmail.com>
2025-03-07 16:18:14 +0530

    created react-vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+ import { Component } from "react"
+import Header from "./components/Header"
+import Category from "./components/category"
+import TopRest from "./components/TopRest"
+import OnlineDelivery from "./components/OnlineDelivery"
+ function App() {
+  return (
+    <>
+      <Header />
+      <Category />
+      <TopRest />
+      <OnlineDelivery/>
+    </>
+  );
+}
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+// Code to render the App component inside the StrictMode
+
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Make sure you're importing from 'react-dom/client'
+import App from './App'; // Adjust the path if needed
+import './index.css'; // Optional: Import your CSS file here
+
+// Create a root for React 18
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component inside the StrictMode
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
